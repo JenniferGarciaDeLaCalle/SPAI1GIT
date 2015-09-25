b2aa970dd271612d31c666638f60af77e96aa331
Jennifer <bm0818@sala7pc18>
2015-09-25 19:45:23 +0200

    Inicial

--- maven/scripts/0.version.bat
+++ maven/scripts/0.version.bat
@@ -1,8 +1,8 @@
 @echo off
-set workspace=C:\DatosJBB\Workspaces\Workspace.Eclipse.JEE.Luna\SPAI\maven
-set PATH=%PATH%C:\Program Files\Java\jdk1.8.0_40\bin;C:\Archivos_de_programa_Portables\apache-maven-3.3.1\bin
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_40
-set M2_HOME=C:\Archivos_de_programa_Portables\apache-maven-3.3.1
+set workspace=C:\Users\bm0818\Desktop\ProyectoSPAI\SPAI\maven
+set PATH=%PATH%C:\Program Files\Java\jdk1.8.0_51\bin;C:\Users\bm0818\Desktop\apache-maven-3.3.3\bin
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_51
+set M2_HOME=C:\Users\bm0818\Desktop\apache-maven-3.3.3
 echo -----------------------------------------
 echo . (C) MIW
 echo -----------------------------------------
@@ -13,6 +13,7 @@ cd %workspace%
 echo ============ mvn --version =======================================================
 echo .
 call mvn --version
+call mvn clean package
 pause
 ::Se queda la consola abierta para realizar comandos en línea
 ::cmd /k
